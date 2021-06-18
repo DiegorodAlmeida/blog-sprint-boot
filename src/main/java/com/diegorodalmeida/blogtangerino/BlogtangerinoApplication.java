db08c24d060c2ad02c510401c3da7749f17b7603
Diego Almeida <diegorodalmeida@gmail.com>
2021-06-18 18:43:57 -0300

    primeiro commit

---
+++ src/main/java/com/diegorodalmeida/blogtangerino/BlogtangerinoApplication.java
@@ -0,0 +1,13 @@
+package com.diegorodalmeida.blogtangerino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogtangerinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogtangerinoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diegorodalmeida/blogtangerino/BlogtangerinoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diegorodalmeida.blogtangerino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogtangerinoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
