60e5e5f120ba6a848436219dd62e1330d3ce1f78
Diego Almeida <diegorodalmeida@gmail.com>
2021-06-20 11:50:38 -0300

    POST

--- src/main/java/com/diegorodalmeida/blogtangerino/resources/UsuarioResource.java
+++ src/main/java/com/diegorodalmeida/blogtangerino/resources/UsuarioResource.java
@@ -1,15 +1,23 @@
 package com.diegorodalmeida.blogtangerino.resources;
 
+import java.net.URI;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.diegorodalmeida.blogtangerino.domain.Usuario;
+import com.diegorodalmeida.blogtangerino.dto.UsuarioDTO;
 import com.diegorodalmeida.blogtangerino.service.UsuarioService;
+
+
 
 @RestController
 @RequestMapping(value="/usuarios")
@@ -19,10 +27,18 @@ public class UsuarioResource {
 	private UsuarioService service;
 	
 	@GetMapping
-	public ResponseEntity <List<Usuario>> findAll (){
-		
+	public ResponseEntity <List<UsuarioDTO>> findAll() {
 		List<Usuario> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UsuarioDTO> listDto = list.stream().map(x -> new UsuarioDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
+	}
+	
+	@PostMapping
+ 	public ResponseEntity<Void> insert(@RequestBody UsuarioDTO objDto) {
+		Usuario obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 
 }
--- src/main/java/com/diegorodalmeida/blogtangerino/service/UsuarioService.java
+++ src/main/java/com/diegorodalmeida/blogtangerino/service/UsuarioService.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.diegorodalmeida.blogtangerino.domain.Usuario;
+import com.diegorodalmeida.blogtangerino.dto.UsuarioDTO;
 import com.diegorodalmeida.blogtangerino.repository.UsuarioRepository;
+
 
 
 
@@ -18,6 +20,13 @@ public class UsuarioService {
 	
 	public List <Usuario> findAll (){
 		return repo.findAll();
-		
+	}
+	
+	public Usuario insert (Usuario obj) {
+		return repo.insert(obj);
+	}
+	
+	public Usuario fromDTO(UsuarioDTO objDto) { 
+		return new Usuario(objDto.getId(), objDto.getNome(), objDto.getEmail());
 	}
 } 
